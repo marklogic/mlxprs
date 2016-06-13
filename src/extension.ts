a0c0885635c94a9f8d37528b9b866aeec68dbdbd
Daniel Quinlan <daniel.quinlan@marklogic.com>
2016-06-13 16:02:44 +0200

    getDbClient moved to own function

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,12 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as ml from 'marklogic';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let sendXQuery = vscode.commands.registerCommand('extension.sendXQuery', () => {
+    function getDbClient() : ml.DatabaseClient {
+        // TODO: keep a single client open
+        // release and create a new one only if there are changes
         var cfg = vscode.workspace.getConfiguration();
 
         var host = String(cfg.get("marklogic.host"));
@@ -14,19 +14,24 @@ export function activate(context: vscode.ExtensionContext) {
         var pwd = String(cfg.get("marklogic.password"));
         var port = Number(cfg.get("marklogic.port"));
 
-        var query = vscode.window.activeTextEditor.document.getText();
-
-        var db = ml.createDatabaseClient({
-          host: host, port: 8000, user: user, password: pwd,
+        return ml.createDatabaseClient({
+          host: host, port: port, user: user, password: pwd,
           authType: 'DIGEST'
-        });
+        })
+    };
+
+    let sendXQuery = vscode.commands.registerCommand('extension.sendXQuery', () => {
+        var query = vscode.window.activeTextEditor.document.getText();
+        var db = getDbClient();
         db.xqueryEval(query).result(
             function(response) {
                 vscode.window.showInformationMessage(JSON.stringify(response));
+                db.release();
             },
             function (error) {
                 vscode.window.showErrorMessage(JSON.stringify(error.body.errorResponse.message));
                 console.error(JSON.stringify(error));
+                db.release();
             });
     });
     context.subscriptions.push(sendXQuery);
