02a5fa00c621224308ea2277c145b70961bc29a9
Daniel Quinlan <daniel.quinlan@marklogic.com>
2016-06-26 12:22:44 +0200

    Queries themselves made synrcronous, set stage for TextDocumentContentProvider

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,8 @@ import * as ml from 'marklogic';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    context.globalState.update("mldbClient", null);
+    const mldbClient = 'mldbClient';
+    context.globalState.update(mldbClient, <ml.DatabaseClient>null);
 
     /**
      * marklogicVSClient
@@ -35,15 +36,28 @@ export function activate(context: vscode.ExtensionContext) {
         var port = Number(cfg.get("marklogic.port"));
         var dbName = String(cfg.get("marklogic.documentsDb"));
 
-        if (context.globalState.get("mldbClient") === null) {
-            context.globalState.update(
-                "mldbClient",
-                new marklogicVSClient(host, port, user, pwd, dbName));
+        if (context.globalState.get(mldbClient) === null) {
+            var newClient = new marklogicVSClient(host, port, user, pwd, dbName);
+            try {
+                context.globalState.update(mldbClient, newClient);
+            } catch(e) {
+                console.log(JSON.stringify(e));
+            }
         };
         return context.globalState.get<marklogicVSClient>("mldbClient");
     };
 
-    let sendXQuery = vscode.commands.registerCommand('extension.sendXQuery', () => {
+    /**
+     * QueryResultsContentProvider implements vscode.TextDocumentContentProvider
+     */
+    class QueryResultsContentProvider implements vscode.TextDocumentContentProvider {
+        public provideTextDocumentContent(uri: vscode.Uri): string {
+            return "12";
+        }
+
+    };
+
+    function _sendXQuery() : Object {
         let actualQuery = vscode.window.activeTextEditor.document.getText();
         let db = getDbClient();
 
@@ -61,28 +75,38 @@ export function activate(context: vscode.ExtensionContext) {
             'return xdmp:eval($actualQuery, (), $options)';
         let extVars = <ml.Variables>{'actualQuery': actualQuery, 'documentsDb': db.dbName};
 
+        var responsePayload;
         db.mldbClient.xqueryEval(query, extVars).result(
             function(response) {
                 vscode.window.showInformationMessage(JSON.stringify(response));
+                responsePayload = response;
             },
             function (error) {
                 vscode.window.showErrorMessage(JSON.stringify(error.body.errorResponse.message));
                 console.error(JSON.stringify(error));
             });
-    });
+            return responsePayload;
+    };
 
-    let sendJSQuery = vscode.commands.registerCommand('extension.sendJSQuery', () => {
-        var query = vscode.window.activeTextEditor.document.getText();
+    function _sendJSQuery() : Object {
+        var actualQuery = vscode.window.activeTextEditor.document.getText();
         var db = getDbClient();
-        db.mldbClient.eval(query).result(
+        var responsePayload;
+        db.mldbClient.eval(actualQuery).result(
             function(response) {
                 vscode.window.showInformationMessage(JSON.stringify(response));
+                responsePayload = response;
             },
             function (error) {
                 vscode.window.showErrorMessage(JSON.stringify(error.body.errorResponse.message));
                 console.error(JSON.stringify(error));
             });
-    });
+        return responsePayload;
+    };
+
+    let sendXQuery = vscode.commands.registerCommand('extension.sendXQuery', () => {_sendXQuery()});
+
+    let sendJSQuery = vscode.commands.registerCommand('extension.sendJSQuery', () => {_sendJSQuery()});
 
     context.subscriptions.push(sendXQuery);
     context.subscriptions.push(sendJSQuery);
