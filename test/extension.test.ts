c6cea9fd270d81806922eb35e600a07b540422a1
Daniel Quinlan <daniel.quinlan@marklogic.com>
2018-02-10 17:22:23 +0100

    First couple of tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -10,13 +10,26 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
+import * as serverTools from '../src/lib/serverTools';
 
 // Defines a Mocha test suite to group tests of similar kind together
-// suite("Extension Tests", () => {
+suite("Extension Tests", () => {
 
-//     // Defines a Mocha unit test
-//     test("Something 1", () => {
-//         assert.equal(-1, [1, 2, 3].indexOf(5));
-//         assert.equal(-1, [1, 2, 3].indexOf(0));
-//     });
-// });
+    // Defines a Mocha unit test
+    test("Something 1", () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+
+    test("getting MarkLogic namespace hints", () => {
+        let allMlN = serverTools.allMlNamespaces
+        assert.notEqual(serverTools.allMlNamespaces, 0)
+    });
+
+    test("getting MarkLogic function hints", () => {
+        let AllMlF = serverTools.allMlFunctions
+        assert.ok(serverTools.allMlFunctions)
+        assert.notEqual(AllMlF.length, 0)
+        assert.notEqual(AllMlF.length, 60)
+    });
+});
