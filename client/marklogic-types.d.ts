2e9e3d2e23cd19c87bd734475ee23cab8785fb19
Daniel Quinlan <Daniel.Quinlan@marklogic.com>
2019-10-21 10:59:23 +0200

    eslint types.d.ts

--- client/marklogic-types.d.ts
+++ client/marklogic-types.d.ts
@@ -1,19 +1,19 @@
 declare module 'marklogic' {
 
   export interface ResultProvider<R> extends NodeJS.ReadWriteStream {
-    result<U>(onFulfilled:  (value: R) => Promise<U>, onRejected:  (error: any) => Promise<U>, onProgress?: (note: any) => any): Promise<U>
-    result<U>(onFulfilled:  (value: R) => Promise<U>, onRejected?: (error: any) => U,          onProgress?: (note: any) => any): Promise<U>
-    result<U>(onFulfilled:  (value: R) => U,          onRejected:  (error: any) => Promise<U>, onProgress?: (note: any) => any): Promise<U>
-    result<U>(onFulfilled?: (value: R) => U,          onRejected?: (error: any) => U,          onProgress?: (note: any) => any): Promise<U>
+    result<U>(onFulfilled:  (value: R) => Promise<U>, onRejected:  (error: any) => Promise<U>, onProgress?: (note: any) => any): Promise<U>;
+    result<U>(onFulfilled:  (value: R) => Promise<U>, onRejected?: (error: any) => U,          onProgress?: (note: any) => any): Promise<U>;
+    result<U>(onFulfilled:  (value: R) => U,          onRejected:  (error: any) => Promise<U>, onProgress?: (note: any) => any): Promise<U>;
+    result<U>(onFulfilled?: (value: R) => U,          onRejected?: (error: any) => U,          onProgress?: (note: any) => any): Promise<U>;
   }
 
   export interface DatabaseClient {
     release: () => void;
-    xqueryEval: <U>(query: string, variables?: Variables) => ResultProvider<U>
-    eval: <U>(query: string, variables?: Variables) => ResultProvider<U>
+    xqueryEval: <U>(query: string, variables?: Variables) => ResultProvider<U>;
+    eval: <U>(query: string, variables?: Variables) => ResultProvider<U>;
   }
 
-  export interface connectionParams {
+  export interface ConnectionParams {
     host: string;
     port: number;
     user: string;
@@ -24,8 +24,8 @@ declare module 'marklogic' {
   }
 
   export interface Variables {
-    [name: string]: number|string|boolean
+    [name: string]: number|string|boolean;
   }
 
-  export function createDatabaseClient(connectionParams: connectionParams): DatabaseClient
+  export function createDatabaseClient(connectionParams: ConnectionParams): DatabaseClient
 }
