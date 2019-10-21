422ea5ab80d19c490397e87b0893d39ee0a77da4
Daniel Quinlan <Daniel.Quinlan@marklogic.com>
2019-10-21 10:06:04 +0200

    eslint cleanup

--- server/completionsXqy.ts
+++ server/completionsXqy.ts
@@ -1,69 +1,69 @@
-'use strict';
+'use strict'
 
 import {
-  CompletionItem, CompletionItemKind
-} from 'vscode-languageserver';
+    CompletionItem, CompletionItemKind
+} from 'vscode-languageserver'
 import {
-  MarkLogicFnDocsObject, MarkLogicParamsObject
-} from './completionTypes';
+    MarkLogicFnDocsObject, MarkLogicParamsObject
+} from './completionTypes'
 
-let xqyHints = require('./etc/marklogic-hint-docs.json').xquery;
-let allMlXqyNamespaces: CompletionItem[] = Object.keys(xqyHints).map((ns) => {
-  let ci: CompletionItem = {
-      label: ns,
-      kind: CompletionItemKind.Class,
-      data: ns + ".namespace"
-  }
-  return ci
+const xqyHints = require('./etc/marklogic-hint-docs.json').xquery
+const allMlXqyNamespaces: CompletionItem[] = Object.keys(xqyHints).map((ns) => {
+    const ci: CompletionItem = {
+        label: ns,
+        kind: CompletionItemKind.Class,
+        data: ns + '.namespace'
+    }
+    return ci
 })
 
-function mlFnDoc2CompletionItem(docObject: MarkLogicFnDocsObject): CompletionItem {
-  let nssep: string = ":"
-  let completionItem: CompletionItem = {
-      label: `${docObject.prefix}${nssep}${docObject.name}()`,
-      kind: CompletionItemKind.Function,
-      documentation: docObject.summary,
-      detail: buildFullFunctionSignature(docObject),
-      insertText: buildFunctionCompletion(docObject),
-      data: docObject
-  }
-  return completionItem
-}
-
-function allMlXqyFunctions(namespace: string): CompletionItem[] {
-  let theseHints: MarkLogicFnDocsObject[] = xqyHints[namespace] || [];
-  return [].concat.apply(
-      [],
-      Object.keys(theseHints).map((fn) => {
-          let hint: MarkLogicFnDocsObject = new MarkLogicFnDocsObject(theseHints[fn]);
-          if (hint.return !== null) {
-              let ci: CompletionItem = mlFnDoc2CompletionItem(hint)
-              return ci;
-          } else return {label: 'dep'};
-      })
-  ).filter((h: CompletionItem) => {return h.label !== 'dep'});
-}
-
 function buildFunctionCompletion(docObject: MarkLogicFnDocsObject): string {
-  let neededParams: MarkLogicParamsObject[] = docObject.params.filter(p => {return p.optional !== true})
-  let optionParams: MarkLogicParamsObject[] = docObject.params.filter(p => {return p.optional === true})
-  let neededParamsString = neededParams.map(p => {return  '$'+p.name    }).join(', ')
-  let optionParamsString = optionParams.map(p => {return '[$'+p.name+']'}).join(', ')
-  let middleComma = ''; if (neededParams.length > 0 && optionParams.length > 0) middleComma = ', '
-  return `${docObject.name}(${neededParamsString}${middleComma}${optionParamsString})`
+    const neededParams: MarkLogicParamsObject[] = docObject.params.filter(p => { return p.optional !== true })
+    const optionParams: MarkLogicParamsObject[] = docObject.params.filter(p => { return p.optional === true })
+    const neededParamsString = neededParams.map(p => { return '$' + p.name }).join(', ')
+    const optionParamsString = optionParams.map(p => { return '[$' + p.name + ']' }).join(', ')
+    let middleComma = ''; if (neededParams.length > 0 && optionParams.length > 0) middleComma = ', '
+    return `${docObject.name}(${neededParamsString}${middleComma}${optionParamsString})`
 }
 
 function buildFullFunctionSignature(docObject: MarkLogicFnDocsObject): string {
-  let neededParams: MarkLogicParamsObject[] = docObject.params.filter(p => {return p.optional !== true})
-  let optionParams: MarkLogicParamsObject[] = docObject.params.filter(p => {return p.optional === true})
-  let neededParamsString = neededParams.map(p => {return  '$'+p.name+' as '+p.type    }).join(",\n\t")
-  let optionParamsString = optionParams.map(p => {return '[$'+p.name+' as '+p.type+']'}).join(",\n\t")
-  let middleComma = ''; if (neededParams.length > 0 && optionParams.length > 0) middleComma = ",\n\t"
-  let nothing: string = docObject.params.length ? "\n\t" : ""
-  return `${docObject.prefix}:${docObject.name}(${nothing}${neededParamsString}${middleComma}${optionParamsString})
+    const neededParams: MarkLogicParamsObject[] = docObject.params.filter(p => { return p.optional !== true })
+    const optionParams: MarkLogicParamsObject[] = docObject.params.filter(p => { return p.optional === true })
+    const neededParamsString = neededParams.map(p => { return '$' + p.name + ' as ' + p.type }).join(',\n\t')
+    const optionParamsString = optionParams.map(p => { return '[$' + p.name + ' as ' + p.type + ']' }).join(',\n\t')
+    let middleComma = ''; if (neededParams.length > 0 && optionParams.length > 0) middleComma = ',\n\t'
+    const nothing: string = docObject.params.length ? '\n\t' : ''
+    return `${docObject.prefix}:${docObject.name}(${nothing}${neededParamsString}${middleComma}${optionParamsString})
   as ${docObject.return}`
 }
 
+
+function mlFnDoc2CompletionItem(docObject: MarkLogicFnDocsObject): CompletionItem {
+    const nssep = ':'
+    const completionItem: CompletionItem = {
+        label: `${docObject.prefix}${nssep}${docObject.name}()`,
+        kind: CompletionItemKind.Function,
+        documentation: docObject.summary,
+        detail: buildFullFunctionSignature(docObject),
+        insertText: buildFunctionCompletion(docObject),
+        data: docObject
+    }
+    return completionItem
+}
+
+function allMlXqyFunctions(namespace: string): CompletionItem[] {
+    const theseHints: MarkLogicFnDocsObject[] = xqyHints[namespace] || []
+    return [].concat(
+        ...Object.keys(theseHints).map((fn) => {
+            const hint: MarkLogicFnDocsObject = new MarkLogicFnDocsObject(theseHints[fn])
+            if (hint.return !== null) {
+                const ci: CompletionItem = mlFnDoc2CompletionItem(hint)
+                return ci
+            } else return { label: 'dep' }
+        })
+    ).filter((h: CompletionItem) => { return h.label !== 'dep' })
+}
+
 export {
-  allMlXqyNamespaces, allMlXqyFunctions
+    allMlXqyNamespaces, allMlXqyFunctions
 }
